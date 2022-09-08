0d424985f7c778aab09a1ef31f52d92644846116
Theo Janke-Furman <tjankefurman@gmail.com>
2022-09-08 15:49:31 -0500

    Finished server-side GET request

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,11 +1,20 @@
 const express = require('express');
-const pool = require('../modules/pool');
 const koalaRouter = express.Router();
 
 // DB CONNECTION
-
+const pool = require('../modules/pool');
 
 // GET
+koalaRouter.get('/', (req, res => {
+    let queryText = 'SELECT * FROM "koalaHolla" ORDER BY "name";'
+    pool.query(queryText).then(result => {
+        res.setEncoding(result.rows);
+    })
+    .catch(error => {
+        console.log('error GETing koalas', error);
+        res.sendStatus(500);
+    })
+}));
 
 
 // POST
